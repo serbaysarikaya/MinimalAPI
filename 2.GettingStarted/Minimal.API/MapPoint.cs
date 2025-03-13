a55c661405908f98b1a93ec0efa1110c428e5eae
Serbay <ssarikaya@tersan.com.tr>
2025-03-13 15:32:52 +0300

    Fixed Update

---
+++ 2.GettingStarted/Minimal.API/MapPoint.cs
@@ -0,0 +1,28 @@
+﻿namespace Minimal.API
+{
+    public class MapPoint
+    {
+        public double x { get; set; }
+        public double y { get; set; }
+        public static bool TryParse(string? value, out MapPoint? result)
+        {
+            try
+            {
+                var splitValue = value?.Split(",").Select(double.Parse).ToArray();
+                result = new MapPoint()
+                {
+                    x = splitValue![0],
+                    y = splitValue[1]
+                };
+                return true;
+            }
+            catch (Exception)
+            {
+
+                result = null;
+                return false;
+            }
+        }
+    }
+
+}
--- 2.GettingStarted/Minimal.API/Program.cs
+++ 2.GettingStarted/Minimal.API/Program.cs
@@ -1,12 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
 using Minimal.API;
+using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<GuidGenerator>();
 builder.Services.AddScoped<PeopleService>();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers();
+
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapGet("get-example", () => "Hola froma getto");
 app.MapPost("post-example", () => "Hola froma getto");
@@ -65,10 +75,10 @@ app.MapGet("people/search", (string? searchTerm, PeopleService peopleService) =>
 });
 
 app.MapGet("mix/{routeParams}", (
-    
-    [FromRoute]string routeParams, 
-    [FromQuery(Name ="q")]int queryParams, 
-    [FromServices]GuidGenerator quidGenerator) =>
+
+    [FromRoute] string routeParams,
+    [FromQuery(Name = "q")] int queryParams,
+    [FromServices] GuidGenerator quidGenerator) =>
 {
     return $"Route param: {routeParams}, Query param: {queryParams}, Guid: {quidGenerator.NewGuid}";
 });
@@ -80,7 +90,59 @@ app.MapPost("people", (Person person) =>
 });
 
 
+app.MapGet("httpContext", async context =>
+{
+    await context.Response.WriteAsync("Hello from http context");
 
 
+});
+
+app.MapGet("http", async (HttpRequest request, HttpResponse response) =>
+{
+    var queryString = request.QueryString;
+    await response.WriteAsync($"Query string: {queryString}");
+});
+
+app.MapGet("claims", (ClaimsPrincipal user) =>
+{
+    var claims = user.Claims.ToList();
+    return Results.Ok(claims);
+});
+
+app.MapGet("cancel", (CancellationToken cancellationToken) =>
+{
+
+    return Results.Ok(cancellationToken);
+});
+
+
+app.MapGet("get-point", (MapPoint point) =>
+{
+    return Results.Ok(point);
+});
+
+app.MapPost("post-point", (MapPoint point) =>
+{
+    return Results.Ok(point);
+});
+
+
+
+app.MapGet("simple-string", () => "Hello world");
+app.MapGet("jwt-raw-obj", () => new { Message = "Hello World" });
+app.MapGet("ok-obj", () => Results.Ok("Hello..."));
+app.MapGet("text-string", () => Results.Json(new { Message = "Hello World" }));
+app.MapGet("redirect", () => Results.Redirect("https://www.google.com/"));
+app.MapGet("download", () => Results.File("./myfile.txt"));
+
+
+app.MapGet("logging", (ILogger<Program> logger) =>
+{
+    logger.LogInformation("Hello from endpoint");
+    return Results.Ok();
+});
+
+
+app.MapControllers();
 
 app.Run();
